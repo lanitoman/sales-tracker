111adf120b6505ee6ac47a7c0c065901d40870d3
lanitoman <lanitomanmd@gmail.com>
2026-07-05 15:40:17 +0400

    init commit

---
+++ src/main/java/com/lanitoman/salestracker/SalesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.lanitoman.salestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesTrackerApplication.class, args);
+    }
+
+}
